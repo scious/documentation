0abac44d03f034c3964d24b4d94abd664914962b
zeluspudding <zeluspudding@users.noreply.github.com>
2022-09-06 17:41:46 -0500

    Remove stock pngs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Scious Docs',
-  // tagline: 'Dinosaurs are cool',
   url: 'https://docs.scious.io',
   baseUrl: '/',
   baseUrlIssueBanner: true,
